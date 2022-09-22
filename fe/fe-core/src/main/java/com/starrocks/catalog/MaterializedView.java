b8d0d27dcb20f77892ba602812c518226f9cb240
ABingHuang <codekhuang@163.com>
2022-09-22 17:04:30 +0800

    optimize framework

--- fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/MaterializedView.java
@@ -24,7 +24,7 @@ import com.starrocks.sql.analyzer.RelationFields;
 import com.starrocks.sql.analyzer.RelationId;
 import com.starrocks.sql.analyzer.Scope;
 import com.starrocks.sql.optimizer.Utils;
-import com.starrocks.sql.optimizer.rule.transformation.materialize.MaterializationContext;
+import com.starrocks.sql.optimizer.rule.transformation.materialization.MaterializationContext;
 import com.starrocks.thrift.TTableDescriptor;
 import com.starrocks.thrift.TTableType;
 import org.apache.logging.log4j.LogManager;
