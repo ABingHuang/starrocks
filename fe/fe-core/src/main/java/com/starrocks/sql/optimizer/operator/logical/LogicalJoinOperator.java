da697fdd8a7dfa33f327b0b53e7cd8e2137050b9
ABingHuang <codekhuang@163.com>
2022-10-11 20:55:11 +0800

    fix nullptr

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/logical/LogicalJoinOperator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/operator/logical/LogicalJoinOperator.java
@@ -163,7 +163,7 @@ public class LogicalJoinOperator extends LogicalOperator {
                 ", Predicate = " + predicate +
                 ", hashcode = " + hashCode() +
                 ", columnRefSet = " + getRequiredChildInputColumns() +
-                projection != null ? "projection:"
+                projection != null && projection.getColumnRefMap() != null ? "projection:"
                 + projection.getColumnRefMap().keySet() + ":" + projection.getColumnRefMap().values() : "" +
                 '}';
     }
